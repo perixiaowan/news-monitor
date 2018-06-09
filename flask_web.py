e5bdd48cdfaaa2c76f8445d6bd79e13cffb78f03
perixiaowan <peri.xiaowan@gmail.com>
2018-06-09 18:39:27 +0800

    update host

--- flask_web.py
+++ flask_web.py
@@ -67,4 +67,4 @@ def getnewdata():
 
 if __name__ == "__main__":
     # app.run(host="0.0.0.0", port=8888, debug=True)
-    app.run(host="127.0.0.1", port=8888, debug=True)
+    app.run(host="0.0.0.0", port=8888, debug=True)
